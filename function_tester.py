00fbece1d5369ba9ec3d1bfa267e5e7a7adc26c4
Jon Withers <jon.s.withers@gmail.com>
2018-06-21 15:55:23 -0400

    Added function testing and exploration, plus a gitignore

---
+++ function_tester.py
@@ -0,0 +1,7 @@
+from post_scraping_text_processing import remove_and_reg
+
+text = """this     is   eval(ez_write_tag([[336,280],'alternatifim_com-box-4','ezslot_2']));  [ reklamı gizle   hide ads ] is
+
+working"""
+
+print(remove_and_reg(text))
