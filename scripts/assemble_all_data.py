8c3d2f3adbc3f95dcc1a3e91f411bde2ed7e44b3
Jon Withers <jon.s.withers@gmail.com>
2018-07-17 10:15:25 -0400

    Lots of verification

--- scripts/assemble_all_data.py
+++ scripts/assemble_all_data.py
@@ -9,10 +9,11 @@ def assemble(df):
     df = clean_bad_language(df)
     df['year'] = pd.to_datetime(df['release']).dt.year
     df['written_before_2002'] = df['year'].map(lambda x: 1 if x <2002 else 0)
-    df['written_before_2004'] = df['year'].map(lambda x: 1 if x <2002 else 0)
-    df['written_before_2006'] = df['year'].map(lambda x: 1 if x <2002 else 0)
-    df['written_before_2008'] = df['year'].map(lambda x: 1 if x <2002 else 0)
-    df.to_csv('../assets/data/assembled_20180711.csv', index = True)
+    df['written_before_2004'] = df['year'].map(lambda x: 1 if x <2004 else 0)
+    df['written_before_2006'] = df['year'].map(lambda x: 1 if x <2006 else 0)
+    df['written_before_2008'] = df['year'].map(lambda x: 1 if x <2008 else 0)
+    df['written_after_2016'] = df['year'].map(lambda x: 1 if x >2016 else 0)
+    df.to_csv('../assets/data/assembled_20180716.csv', index = True)
 
 if __name__ == "__main__":
     df = pd.read_csv("../assets/lyrics/master_data_20180626.csv", index_col = 0)
